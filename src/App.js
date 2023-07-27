656a7954a84562bd71da75f46fc755ea88a5227a
saifmohamedsv <xcyberghostx1@gmail.com>
2023-07-27 13:13:12 +0300

    handle search on mount

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
-import SearchBar from './components/SearchBar'
-import searchImages from './api'
-import ImageList from './components/ImageList'
-import { useState, useEffect } from 'react'
+import SearchBar from "./components/SearchBar";
+import searchImages from "./api";
+import ImageList from "./components/ImageList";
+import { useState, useEffect } from "react";
 
-function App(){
+function App() {
+  const [images, setImages] = useState([]);
 
-    const [images, setImages] = useState([])
+  const handleSubmit = async (term) => {
+    const result = await searchImages(term);
+    setImages(result);
+  };
 
-    const handleSubmit = async (term) => {
-        const result = await searchImages(term)
-        setImages(result)
-    }
+  useEffect(() => {
+    handleSubmit("marvel");
+  }, []);
 
-    useEffect(() => {
-        console.log('working!')
-    }, [])
-
-    return(
-        <div>
-            <SearchBar onSubmit={handleSubmit}/>
-            <ImageList images={images} />
-        </div>
-    )
+  return (
+    <div>
+      <SearchBar onSubmit={handleSubmit} />
+      <ImageList images={images} />
+    </div>
+  );
 }
 
-export default App
+export default App;
